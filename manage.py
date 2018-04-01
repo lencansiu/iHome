dab772ea1c7c31fe4f8f9a1e7bc680316fab18ac
xwp <xwp_fullstack@163.com>
2018-04-01 16:01:21 +0800

    配置mysql数据库

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 class Config(object):
@@ -10,11 +11,17 @@ class Config(object):
     # 开启调试模式
     DEBUG = True
 
+    # 配置mysql数据库: 开发中使用真实IP
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/iHome_07'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 app = Flask(__name__)
 
 # 加载配置
 app.config.from_object(Config)
+# 创建连接到mysql数据库的对象
+db = SQLAlchemy(app)
 
 
 @app.route('/')
