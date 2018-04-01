7f22c611d10a24f4adcb870fccd7d2ae20e1f545
xwp <xwp_fullstack@163.com>
2018-04-01 21:29:59 +0800

    使用flask_session扩展来存储session数据到redis,并配置Session

--- manage.py
+++ manage.py
@@ -7,6 +7,8 @@ import redis
 from flask_wtf.csrf import CSRFProtect
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+# session在flask中的扩展包
+from flask_session import Session
 
 
 class Config(object):
@@ -25,6 +27,15 @@ class Config(object):
     # 配置redis数据库: 开发中使用真实IP
     REDIS_HOST = '127.0.0.1'
     REDIS_PORT = 6379
+
+    # 配置session数据存储到redis数据库
+    SESSION_TYPE = 'redis'
+    # 指定存储session数据的redis的位置
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 开启session数据的签名，意思是让session数据不以明文形式存储
+    SESSION_USE_SIGNER = True
+    # 设置session的会话的超时时长：一天
+    PERMANENT_SESSION_LIFETIME = 3600 * 24
 
 
 app = Flask(__name__)
@@ -49,12 +60,19 @@ Migrate(app, db)
 # 将数据库迁移脚本添加到脚本管理器
 manager.add_command('db', MigrateCommand)
 
+# 使用session在flask扩展实现将session数据存储在redis
+Session(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
     # 测试redis数据库
     redis_store.set('name', 'sz07')
+
+    # 测试session: flask自带的session模块，用于存储session
+    from flask import session
+    session['name'] = 'sz07sz07'
 
     return 'index'
 
