1514dd085b73b4b9d2b89ecaa88dab9a6b05fc48
xwp <xwp_fullstack@163.com>
2018-04-01 15:45:58 +0800

    加载配置

--- manage.py
+++ manage.py
@@ -4,7 +4,17 @@
 from flask import Flask
 
 
+class Config(object):
+    """加载配置 所有的配置信息都写在这个类里面"""
+
+    # 开启调试模式
+    DEBUG = True
+
+
 app = Flask(__name__)
+
+# 加载配置
+app.config.from_object(Config)
 
 
 @app.route('/')
@@ -13,4 +23,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
